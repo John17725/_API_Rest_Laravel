2c18ebe6c0d0ee93c3a7a62c0db48ed22d12f9e1
John17725 <barthjoss@hotmail.es>
2022-03-30 00:21:33 -0600

    add auth and login to api

--- API/routes/api.php
+++ API/routes/api.php
@@ -5,7 +5,16 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\V1\PostController as PostV1;
 use App\Http\Controllers\Api\V2\PostController as PostV2;
+use App\Http\Controllers\Api\LoginController;
 
-Route::apiResource('v1/posts', PostV1::class)->only(['show','index','destroy']);
+Route::apiResource('v1/posts', PostV1::class)
+    ->only(['show','index','destroy'])
+    ->middleware('auth:sanctum');
 
-Route::apiResource('v2/posts', PostV2::class)->only(['show','index']);
+Route::apiResource('v2/posts', PostV2::class)
+    ->only(['show','index'])
+    ->middleware('auth:sanctum');
+
+Route::post('login', [LoginController::class, 
+    'login'
+]);
